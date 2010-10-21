08b6029c8d91279b850a46d7d138152c410f6f5e
Martin Peschke <martin@friendfund.com>
2010-10-21 09:47:12 +0000

    removing dot from accepted pool urls

--- friendfund/config/routing.py
+++ friendfund/config/routing.py
@@ -22,20 +22,20 @@ def make_map(config):
 	# CUSTOM ROUTES HERE
 	map.connect('/pool/start', controller='pool', action='reset')
 	map.connect('/pool/create', controller='pool', action='create')
-	map.connect('get_pool', '/pool/{pool_url}', controller='pool', action='index', requirements={'pool_url': '[0-9a-zA-Z.~_-]+'})
-	map.connect('chipin', '/pool/{pool_url}/chipin', controller='contribution', action="chipin", requirements={'pool_url': '[0-9a-zA-Z.~_-]+'})
-	map.connect('chipin_fixed', '/pool/{pool_url}/chipin_fixed', controller='contribution', action="chipin_fixed", requirements={'pool_url': '[0-9a-zA-Z.~_-]+'})
-	map.connect('pool_action', '/pool/{pool_url}/{action}', controller='pool', requirements={'pool_url': '[0-9a-zA-Z.~_-]+'})
+	map.connect('get_pool', '/pool/{pool_url}', controller='pool', action='index', requirements={'pool_url': '[0-9a-zA-Z~_-]+'})
+	map.connect('chipin', '/pool/{pool_url}/chipin', controller='contribution', action="chipin", requirements={'pool_url': '[0-9a-zA-Z~_-]+'})
+	map.connect('chipin_fixed', '/pool/{pool_url}/chipin_fixed', controller='contribution', action="chipin_fixed", requirements={'pool_url': '[0-9a-zA-Z~_-]+'})
+	map.connect('pool_action', '/pool/{pool_url}/{action}', controller='pool', requirements={'pool_url': '[0-9a-zA-Z~_-]+'})
 	
 	map.connect('/invite/friends', controller='invite', action='friends')
 	
-	map.connect('invite', '/invite/{pool_url}/{method}', controller='invite', action='method', requirements={'pool_url': '[0-9a-zA-Z.~_-]+', 'method':CONNECT_METHODS})
-	map.connect('invite_index', '/invite/{pool_url}', controller='invite', action='display', requirements={'pool_url': '[0-9a-zA-Z.~_-]+'})
-	map.connect('invite_action', '/invite/{pool_url}/{action}', controller='invite', requirements={'pool_url': '[0-9a-zA-Z.~_-]+'})
+	map.connect('invite', '/invite/{pool_url}/{method}', controller='invite', action='method', requirements={'pool_url': '[0-9a-zA-Z~_-]+', 'method':CONNECT_METHODS})
+	map.connect('invite_index', '/invite/{pool_url}', controller='invite', action='display', requirements={'pool_url': '[0-9a-zA-Z~_-]+'})
+	map.connect('invite_action', '/invite/{pool_url}/{action}', controller='invite', requirements={'pool_url': '[0-9a-zA-Z~_-]+'})
 	map.connect('/receiver/{method}', controller='receiver', action='method', requirements={'method':CONNECT_METHODS})
 	map.connect('mybadges', '/mybadges/panel/{badge_name}', controller="mybadges", action="panel")
 	map.connect('/myprofile/{action}/{token}', controller="myprofile")
-	map.connect('/{controller}/{pool_url}/{action}', controller='pool', requirements={'pool_url': '[0-9a-zA-Z.~_-]+'})
+	map.connect('/{controller}/{pool_url}/{action}', controller='pool', requirements={'pool_url': '[0-9a-zA-Z~_-]+'})
 	map.connect('/{controller}/{action}')
 	map.connect('/{controller}/{action}/{id}')
 	
