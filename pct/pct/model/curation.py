fe5cdef7d784e1c21ba13527cb7f3d515d34afd2
Martin Peschke <martin@friendfund.com>
2010-10-21 19:47:34 +0000

    some first style attempts

--- pct/pct/model/curation.py
+++ pct/pct/model/curation.py
@@ -1,6 +1,10 @@
 from friendfund.model.mapper import DBMappedObject, DBCDATA, GenericAttrib, DBMapper, DBMapping
 from friendfund.model.product import Product
 
+images = ['aff_program_logo_url', 'picture_small', 'picture_large', 'product_picture_url']
+key_order = filter(lambda x: x not in images, sorted(map(lambda x: x.pykey, Product._keys)))
+
+
 class ProductVersion(DBMappedObject):
 	_set_root = _get_root = 'PRODUCT_VERSION'
 	_set_proc = _get_proc = None
@@ -25,6 +29,8 @@ class GetCurationQueue(DBMappedObject):
 	"""
 		exec cur.get_curation_products '<PRODUCT_CURATION region="DE" type="INSERT" page_no="0"/>'
 	"""
+	key_order = key_order
+	image_keys = images
 	_set_root = 'PRODUCT_CURATION'
 	_get_root = 'CURATION'
 	_set_proc = _get_proc = "cur.get_curation_products"
