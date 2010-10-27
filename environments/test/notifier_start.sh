e44a8bc3739011c4d4d5e2a25c52351d4039b281
Martin Peschke <martin@friendfund.com>
2010-10-27 18:09:24 +0000

    environment test updates

--- environments/test/notifier_start.sh
+++ environments/test/notifier_start.sh
@@ -1,2 +1,3 @@
-export PYTHONPATH="/opt/www/ff_test";
-python friendfund/tasks/notifier.py -f test_env.ini
+export PYTHONPATH="/home/www-data/ff_test:/opt/www/ff_test"
+python /home/www-data/ff_test/friendfund/tasks/notifier.py -f test_env.ini
+
