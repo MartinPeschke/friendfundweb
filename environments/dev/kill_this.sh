8f3efbe729838d09038981933c62db9b37af719a
Martin Peschke <martin@friendfund.com>
2010-10-28 08:50:35 +0000

    adding ssl_protocol

--- environments/dev/kill_this.sh
+++ environments/dev/kill_this.sh
@@ -1,8 +1,4 @@
 /etc/init.d/memcached_dev restart
-ps auxww | grep paster | awk '{print $2}' | xargs kill -9
-ps auxww | grep celeryd_dev | awk '{print $2}' | xargs kill -9
-
-/opt/www/friendfund/celery_start.sh &
-/opt/www/ff_test/startup.sh
+/opt/www/friendfund/celeryd.sh restart
 
 
