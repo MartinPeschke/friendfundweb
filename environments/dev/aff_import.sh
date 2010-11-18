8d2bbce4034d6cb38a4ec59d9b2df0c967e7d3b7
Martin Peschke <martin@friendfund.com>
2010-11-18 15:08:06 +0000

    added fb api key

--- environments/dev/aff_import.sh
+++ environments/dev/aff_import.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH="/home/www-data/ff_dev:/opt/www/friendfund";
-python /home/www-data/ff_dev/friendfund/tasks/api_crawler_zanox.py -f /opt/www/friendfund/development2.ini -r de
-python /home/www-data/ff_dev/friendfund/tasks/api_crawler_zanox.py -f /opt/www/friendfund/development2.ini -r gb
+#python /home/www-data/ff_dev/friendfund/tasks/api_crawler_zanox.py -f /opt/www/friendfund/development2.ini -r de
+#python /home/www-data/ff_dev/friendfund/tasks/api_crawler_zanox.py -f /opt/www/friendfund/development2.ini -r gb
 python /home/www-data/ff_dev/friendfund/tasks/api_crawler_zanox.py -f /opt/www/friendfund/development2.ini -r us
--- environments/dev/notifier_start.sh
+++ environments/dev/notifier_start.sh
@@ -1,2 +1,6 @@
-export PYTHONPATH="/opt/www/friendfund";
-python friendfund/tasks/notifier.py -f development.ini
+runpath=/opt/www/friendfund
+binpath=/home/www-data/ff_dev
+config=./development.ini
+
+export PYTHONPATH="$runpath:$binpath";
+python $binpath/friendfund/tasks/notifier.py -f $config
