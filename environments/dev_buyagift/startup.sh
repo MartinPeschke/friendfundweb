66f7479936efbd0e9d89e05d317ef27768bc469d
Martin Peschke <martin@friendfund.com>
2011-01-30 16:20:12 +0000

    importing initial config to buyagift

---
+++ environments/dev_buyagift/startup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+exec_name=diariobebe
+runpath=/opt/www/diariobebe
+binpath=/home/www-data/ff_dev
+config=./development.ini
+
+export PYTHONPATH="$runpath:$binpath";
+cd $runpath;
+case "$1" in
+  start)
+    /server/pylons1.0/bin/paster serve --reload --daemon --pid-file=run/${exec_name}.pid --log-file=./logs/webserver_${exec_name}.log $config start
+    ;;
+  stop)
+    /server/pylons1.0/bin/paster serve --reload --daemon --pid-file=run/${exec_name}.pid --log-file=./logs/webserver_${exec_name}.log  $config stop
+    ;;
+  restart)
+    /server/pylons1.0/bin/paster serve --reload --daemon --pid-file=run/${exec_name}.pid --log-file=./logs/webserver_${exec_name}.log $config restart
+    ;;
+  *)
+    echo $"Usage: $0 {start|stop|restart}"
+    exit 1
+esac
