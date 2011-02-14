c55c9a24ecf31c39d0117321750a1f5da086509f
Martin Peschke <martin@friendfund.com>
2011-02-14 16:34:00 +0000

    adding and removing artifacts

--- friendfund/tests/functional/test_customizer.py
+++
@@ -1,7 +0,0 @@
-from friendfund.tests import *
-
-class TestCustomizerController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='customizer', action='index'))
-        # Test response...
