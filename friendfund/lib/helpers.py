fb59d6a9cdce335adbcead99a41a7f321477ef4d
Martin Peschke <martin@friendfund.com>
2011-02-14 16:29:51 +0000

    removing misleading links and unused code

--- friendfund/lib/helpers.py
+++ friendfund/lib/helpers.py
@@ -4,8 +4,6 @@
 Consists of functions to typically be used within templates, but also
 available to Controllers. This module is available to templates as 'h'.
 """
-# Import helpers as desired, or define your own, ie:
-#from webhelpers.html.tags import checkbox, password
 import os, md5
 
 from pylons import config
