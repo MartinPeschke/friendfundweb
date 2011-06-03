13db2a0bb95d55aa39946e0024dc1a2986a15372
Martin Peschke <martin@friendfund.com>
2011-06-03 09:29:46 +0000

    test configuration

--- environments/test/startup.sh
+++ environments/test/startup.sh
@@ -8,13 +8,13 @@ export PYTHONPATH="$runpath:$binpath";
 cd $runpath;
 case "$1" in
   start)
-    /server/pylons1.0/bin/paster serve --reload --daemon --pid-file=run/test.pid --log-file=./logs/test.log $config start
+    /server/pylons1.0/bin/paster serve --reload --daemon --pid-file=run/test.pid --log-file=./logs/webserver.log $config start
     ;;
   stop)
-    /server/pylons1.0/bin/paster serve --reload --daemon --pid-file=run/test.pid --log-file=./logs/test.log  $config stop
+    /server/pylons1.0/bin/paster serve --reload --daemon --pid-file=run/test.pid --log-file=./logs/webserver.log  $config stop
     ;;
   restart)
-    /server/pylons1.0/bin/paster serve --reload --daemon --pid-file=run/test.pid --log-file=./logs/test.log $config restart
+    /server/pylons1.0/bin/paster serve --reload --daemon --pid-file=run/test.pid --log-file=./logs/webserver.log $config restart
     ;;
   *)
     echo $"Usage: $0 {start|stop|restart}"
