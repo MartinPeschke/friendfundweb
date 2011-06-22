9ff9a6c666189b6f21f50ace0621172cad2635fe
git <martin@friendfund.com>
2011-06-22 12:13:17 +0200

    ng version to FF_2.3.1_rc1

--- friendfund/lib/app_globals.py
+++ friendfund/lib/app_globals.py
@@ -21,7 +21,7 @@ log = logging.getLogger(__name__)
 _ = lambda x:x
 
 pyodbc.pooling = False
-SVNREVISION = "SPRINT_2.3.1"
+SVNREVISION = "FF_2.3.1_rc1"
 REVISION_ENDING = md5.md5(SVNREVISION).hexdigest()
 
 
