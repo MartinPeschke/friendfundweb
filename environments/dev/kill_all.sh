289de4b1f94621faac0044dab1487d742f0880d7
git <martin@friendfund.com>
2011-06-23 17:37:03 +0200

    moving config files into respective folder in environments

--- environments/dev/kill_all.sh
+++ environments/dev/kill_all.sh
@@ -3,9 +3,13 @@
 ps auxww | grep paster | awk '{print $2}' | xargs kill -9
 ps auxww | grep celeryd | awk '{print $2}' | xargs kill -9
 
-/opt/www/friendfund/celery_start.sh &
-/opt/www/ff_test/celery_start.sh &
 
-/opt/www/ff_test/startup.sh
+/opt/www/friendfund/celeryd.sh start
+/opt/www/ff_test/celeryd.sh start
+/opt/www/demo/celeryd.sh start
+
+/opt/www/ff_test/startup.sh start
+/opt/www/demo/startup.sh start 
+
 
 
