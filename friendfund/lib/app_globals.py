1e77ef292675f7295e1bb1f838806a1bf8d8dd6f
git <martin@friendfund.com>
2011-06-23 11:38:25 +0200

    bump version for release FF_2.3.1_rc2

--- friendfund/lib/app_globals.py
+++ friendfund/lib/app_globals.py
@@ -21,7 +21,7 @@ log = logging.getLogger(__name__)
 _ = lambda x:x
 
 pyodbc.pooling = False
-SVNREVISION = "FF_2.3.1_rc1"
+SVNREVISION="FF_2.3.1_rc2"
 REVISION_ENDING = md5.md5(SVNREVISION).hexdigest()
 
 
