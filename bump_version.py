1375e0f6ae063860688b0e0295390ea47a40ff9f
root <martin@friendfund.com>
2011-06-24 10:23:09 +0200

    bump script now also modifies setup.py

--- bump_version.py
+++ bump_version.py
@@ -6,6 +6,7 @@ import logging
 import optparse
 
 REV_IDENT = re.compile("^SVNREVISION.*")
+SETUP_REV_IDENT = re.compile("""^(?P<start>\W*version=["'])[^"']+(?P<end>["'],)$""")
 
 def main():
     usage = "usage: %prog [options]"
@@ -17,6 +18,7 @@ def main():
 
     if not options.revision:
         parser.error('Must add -r argument')
+    
     inf = open('./friendfund/lib/app_globals.py', 'r')
     input = inf.readlines()
     output = []
@@ -30,6 +32,24 @@ def main():
         inf.close()
 
     outf = open('./friendfund/lib/app_globals.py', 'w')
+    try:
+        outf.writelines(output)
+    finally:
+        outf.close()
+    
+    inf = open('./setup.py', 'r')
+    input = inf.readlines()
+    output = []
+    try:
+        for line in input:
+            if SETUP_REV_IDENT.match(line):
+                output.append(re.sub(SETUP_REV_IDENT, '\g<start>%s\g<end>' % options.revision, line))
+            else:
+                output.append(line)
+    finally:
+        inf.close()
+
+    outf = open('./setup.py', 'w')
     try:
         outf.writelines(output)
     finally:
