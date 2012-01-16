098fb6c3d24982304eed4bb39fe524589fe97ac9
www-data <martin@friendfund.com>
2012-01-16 14:05:51 +0100

    changing static deploy token

--- friendfund/lib/app_globals.py
+++ friendfund/lib/app_globals.py
@@ -21,7 +21,7 @@ log = logging.getLogger(__name__)
 _ = lambda x:x
 
 pyodbc.pooling = False
-SVNREVISION="FF_2.3.1_rc7_preventing constant reloading"
+SVNREVISION="FF_2.3.1_rc8_more_burda_changes_recipient/cancelpayment/select runtime"
 REVISION_ENDING = md5.md5(SVNREVISION).hexdigest()
 
 
