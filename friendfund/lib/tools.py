d24a33f2ca4a4956b8593204b421e52a57224f5b
Martin Peschke <martin@friendfund.com>
2012-08-23 10:47:25 +0200

    further html santizie relaxations

--- friendfund/lib/tools.py
+++ friendfund/lib/tools.py
@@ -136,9 +136,9 @@ def generate_mnemonic_password(letters=8, digits=4):
 	chars += ''.join([str(random.randrange(0, 9)) for i in range(digits)])
 	return chars
 
-def sanitize_html(html, valid_tags = ['a','strong', 'em', 'p', 'ul', 'ol', 'li', 'br', 'b', 'i', 'u', 's', 'strike', 'font', 'pre', 'blockquote', 'div']
+def sanitize_html(html, valid_tags = ['a','strong', 'em', 'p', 'ul', 'ol', 'li', 'br', 'b', 'i', 'u', 's', 'strike', 'font', 'pre', 'blockquote', 'div', 'span']
 	, strip_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
-	, valid_attrs = ['size', 'color', 'face', 'title', 'align']):
+	, valid_attrs = ['size', 'color', 'face', 'title', 'align', 'style']):
 	soup = BeautifulSoup(html)
 	for tag in soup.findAll(True):
 		if tag.name.lower() not in valid_tags:
