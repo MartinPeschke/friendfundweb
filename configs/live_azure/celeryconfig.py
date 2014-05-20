a3ece2866c50cbed346c4040fb3270f1c1588539
Martin Peschke <martin@hackandcraft.com>
2014-05-20 18:32:30 +0200

    adapt config file path

--- configs/live_azure/celeryconfig.py
+++ configs/live_azure/celeryconfig.py
@@ -23,7 +23,7 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
 
-CELERY_ADDITIONAL_CONFIG="code/config.ini"
+CELERY_ADDITIONAL_CONFIG="code/current/config.ini"
 
 
 # If you're doing mostly I/O you can have more processes,
