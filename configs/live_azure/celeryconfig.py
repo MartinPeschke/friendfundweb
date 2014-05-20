a1ed06447bf20af4d7159dd794e710300819ddee
Martin Peschke <martin@hackandcraft.com>
2014-05-20 18:28:29 +0200

    adapt config file path

--- configs/live_azure/celeryconfig.py
+++ configs/live_azure/celeryconfig.py
@@ -1,8 +1,4 @@
-BROKER_HOST = "127.0.0.1"
-BROKER_PORT = 5672
-BROKER_USER = "rmquser_live"
-BROKER_PASSWORD = "rmqpw_liveHGasegoiAuhqfReo96r"
-BROKER_VHOST = "rmqvhost_live"
+BROKER_URL = "amqp://rabbitmquser:rabbitmqpassword@127.0.0.1:5672/rabbitmqvhost"
 CELERY_RESULT_BACKEND = "amqp"
 
 CELERYD_SOFT_TASK_TIME_LIMIT=480
@@ -12,25 +8,32 @@ CELERY_STORE_ERRORS_EVEN_IF_IGNORED = False
 CELERY_IGNORE_RESULT = True
 CELERY_DISABLE_RATE_LIMITS = True
 
-CELERY_QUEUES = {"default_queue": {
-				"exchange": "celeryevent_live",
-				"exchange_type": "direct",
-				"binding_key": "celeryevent_live_ff",
-				"routing_key": "celeryevent_live_ff"}}
-CELERY_DEFAULT_QUEUE = "default_queue"
+CELERY_QUEUES = {"default_dev": {"exchange": "celeryevent_dev",
+                                 "binding_key": "celeryevent_dev_dev",
+                                 "routing_key": "celeryevent_dev_dev"}}
+
+CELERY_DEFAULT_QUEUE = "default_dev"
+
 CELERY_DEFAULT_EXCHANGE_TYPE = "direct"
-CELERY_DEFAULT_EXCHANGE = "celeryevent_live"
-CELERY_DEFAULT_ROUTING_KEY = "celeryevent_live_ff"
+CELERY_DEFAULT_EXCHANGE = "celeryevent_dev"
+CELERY_DEFAULT_ROUTING_KEY = "celeryevent_dev_dev"
 
 CELERY_EVENT_SERIALIZER = "json"
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ['json']
 
-CELERY_ADDITIONAL_CONFIG="/opt/www/ff_live/live.ini"
+CELERY_ADDITIONAL_CONFIG="code/config.ini"
 
 
 # If you're doing mostly I/O you can have more processes,
 # but if mostly spending CPU, try to keep it close to the
 # number of CPUs on your machine. If not set, the number of CPUs/cores
 # available will be used.
-CELERYD_CONCURRENCY = 8
+CELERYD_CONCURRENCY = 2
 
-CELERYD_LOG_FILE = "logs/celeryd_live.log"
+
+
+# sudo rabbitmqctl add_user rabbitmquser rabbitmqpassword
+# sudo rabbitmqctl add_vhost rabbitmqvhost
+# sudo rabbitmqctl set_permissions -p rabbitmqvhost rabbitmquser ".*" ".*" ".*"
